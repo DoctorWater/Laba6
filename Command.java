cb1a9b303a5843376dca47949bef53e83d46bad1
DoctorWater <malckov.schura2015@gmail.com>
2021-09-08 02:57:09 +0300

    Package has been declared

--- Command.java
+++
@@ -1,7 +0,0 @@
-import java.io.IOException;
-import java.util.Hashtable;
-
-public interface Command {
-    void execute() throws IOException, RecursionExeption, IllegalVarValue;
-    Hashtable<String, Product> returnTable();
-}
--- StructureInterface.java
+++
@@ -1,7 +0,0 @@
-import java.util.Hashtable;
-
-public interface StructureInterface {
-    public String getName();
-    public void setName(String name);
-    public void stringToEnum(String s);
-}
