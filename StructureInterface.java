10ccf93719953173a655b9f1f0e08ecf03f32816
DoctorWater <75388424+DoctorWater@users.noreply.github.com>
2021-09-28 09:11:43 +0300

    Delete StructureInterface.java

--- StructureInterface.java
+++
@@ -1,9 +0,0 @@
-package mainClient.java;
-
-import java.util.Hashtable;
-
-public interface StructureInterface {
-    public String getName();
-    public void setName(String name);
-    public void stringToEnum(String s);
-}
